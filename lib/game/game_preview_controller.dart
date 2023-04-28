8885033654d15d824f7e4208d31038be8eedecea
rpuiu <raz.puiu@gmail.com>
2023-04-28 09:16:51 +0300

    Fix: Joiner is displayed in the host game preview

--- lib/game/game_preview_controller.dart
+++ lib/game/game_preview_controller.dart
@@ -1,11 +1,15 @@
+import 'package:canto_cards_game/db/db_ops.dart';
 import 'package:canto_cards_game/game/game_model.dart';
 import 'package:canto_cards_game/player/player_model.dart';
 import 'package:get/get.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class GamePreviewController extends GetxController {
   Rx<Game> game = Game.empty().obs;
-  late Rx<Player> host = Player.empty().obs;
-  late Rx<Player> joiner = Player.empty().obs;
+  Rx<Player> host = Player.empty().obs;
+  Rx<Player> joiner = Player.empty().obs;
+  DbOps db = Get.find<DbOps>();
+  var channel;
 
   @override
   void onInit() {
@@ -13,5 +17,26 @@ class GamePreviewController extends GetxController {
     game.value = Get.arguments['game'];
     host.value = Get.arguments['host'] ?? host.value;
     joiner.value = Get.arguments['joiner'] ?? joiner.value;
+
+    channel = db.supabase.channel('public:games:id=eq.${game.value.id}').on(
+        RealtimeListenTypes.postgresChanges,
+        ChannelFilter(
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'games',
+          filter: 'id=eq.${game.value.id}',
+        ), (payload, [ref]) async {
+      print('Change received: ${payload.toString()}');
+      joiner.value = await db.getPlayer(payload["new"]["joinerId"]);
+    });
+    channel.subscribe();
+  }
+
+  @override
+  Future<void> onClose() async {
+    super.onClose();
+    host.close();
+    joiner.close();
+    await db.supabase.removeChannel(channel);
   }
 }
