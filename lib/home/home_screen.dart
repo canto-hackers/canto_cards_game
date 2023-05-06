6b75d4c73853e323a116e876ecc30d57460f5f64
rpuiu <raz.puiu@gmail.com>
2023-05-06 11:48:49 +0300

    UI for the home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -10,11 +10,66 @@ class HomeScreen extends GetView<HomeController> {
   Widget build(BuildContext context) {
     controller.init();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Get.toNamed('${Routes.gameLobby}?userId=3'),
-          child: Text("Go to lobby"),
-        ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "images/splash-screen.gif",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.black.withOpacity(0.5),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TweenAnimationBuilder(
+                  tween: Tween<double>(begin: 0, end: 1),
+                  duration: Duration(seconds: 2),
+                  builder: (_, double value, __) {
+                    return Opacity(
+                      opacity: value,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "The Blackout",
+                            style: TextStyle(
+                              fontSize: 40,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Last Hope on Canto",
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () => Get.toNamed('${Routes.gameLobby}?userId=3'),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text("Enter lobby"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
